9febfc2c829d616f63577ce4d431b2b8ab4d6e84
constantin2810 <czgripcea@gmail.com>
2021-11-15 22:17:40 +0000

    commit creating thank-you page and shared page

---
+++ assets/index.js
@@ -0,0 +1,10 @@
+
+        $(document).ready(function() {
+            $('.collapsible').collapsible();
+            $(".button-collapse").sideNav();
+            $(".dropdown-button").dropdown({ hover: true });
+            $('.modal').modal();
+            $('.parallax').parallax();
+            $('.carousel').carousel();
+        });
+    
